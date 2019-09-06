cbadc34bdc1b4ec0f2a75edb09b3abc223b8f9d9
Jeffrey Yau <jeffreyyau@outlook.com>
2019-09-06 22:57:57 +0800

    feat: Add basic clock and function

---
+++ js/script.js
@@ -0,0 +1,23 @@
+let time = new Date();
+let pos_s = time.getSeconds();
+let pos_m = time.getMinutes();
+let pos_h = time.getHours();
+let x = pos_s * 6;
+let y = pos_m * 6;
+let z = pos_h * 30 + pos_m * 0.5;
+document.write(`
+    <style>
+        @keyframes j_s {
+            from {transform: rotate(${x}deg);}
+            to {transform: rotate(${x + 360}deg);}
+        }
+        @keyframes j_m {
+            from {transform: rotate(${y}deg);}
+            to {transform: rotate(${y + 360}deg);}
+        }
+        @keyframes j_h {
+            from {transform: rotate(${z}deg);}
+            to {transform: rotate(${z + 360}deg);}
+        }
+    </style>
+`);
