1ee630fca9a882efe692a46a01d14429d927ee31
Jeffrey Yau <jeffreyyau@outlook.com>
2019-10-03 23:57:18 +0800

    style: Change indentation style to two spaces

--- js/script.js
+++ js/script.js
@@ -6,18 +6,18 @@ let x = pos_s * 6;
 let y = pos_m * 6;
 let z = pos_h * 30 + pos_m * 0.5;
 document.write(`
-    <style>
-        @keyframes j_s {
-            from {transform: rotate(${x}deg);}
-            to {transform: rotate(${x + 360}deg);}
-        }
-        @keyframes j_m {
-            from {transform: rotate(${y}deg);}
-            to {transform: rotate(${y + 360}deg);}
-        }
-        @keyframes j_h {
-            from {transform: rotate(${z}deg);}
-            to {transform: rotate(${z + 360}deg);}
-        }
-    </style>
+  <style>
+      @keyframes j_s {
+          from {transform: rotate(${x}deg);}
+          to {transform: rotate(${x + 360}deg);}
+      }
+      @keyframes j_m {
+          from {transform: rotate(${y}deg);}
+          to {transform: rotate(${y + 360}deg);}
+      }
+      @keyframes j_h {
+          from {transform: rotate(${z}deg);}
+          to {transform: rotate(${z + 360}deg);}
+      }
+  </style>
 `);
